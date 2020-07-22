0d752f817f116f192709a16ad3e59a63004d8376
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-07-21 23:34:08 -0400

    Added commented out sections for App.js for private routing

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+import Dashboard from './components/pages/Dashboard';
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
+    <div>
+      {/* <NavBar /> */}
     <div className="App">
       <h1>Essentialism Project</h1>
+      {/* <Switch>
+       <Route exact path="/">
+         <HomePage />
+       </Route>
+       <Route path="/login">
+         <LoginPage />
+       </Route>
+       <PrivateRoute exact path="/dashboard" component={Dashboard} />
+     </Switch> */}
+    </div>
     </div>
   );
 }
