3b30864092da6264ce9d58bdc5d95e8ebcbca9c1
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-07-23 17:09:31 -0400

    Added toggle functionality to the Values items.  State managed in component and not globally at this time.

--- essentialism/src/components/lists/Values.js
+++ essentialism/src/components/lists/Values.js
@@ -20,48 +20,48 @@ const useStyles = makeStyles({
   },
 });
 
-const data = [
+const initialValue = [
     {
     value: "Weight Loss",
     description: "Improve your health with weight loss.",
     img: 'https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: 1,
-    selected: false,
+    isSelected: false,
     },
     {
     value: "Organization",
     description: "An organized room is an organized mind.",
     img: 'https://images.pexels.com/photos/670723/pexels-photo-670723.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     id: 2,
-    selected: false,
+    isSelected: false,
     },
     {
     value: "Reading",
     description: "For those who love to read or want to read more.",
     img: 'https://images.pexels.com/photos/34075/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     id: 3,
-    selected: false,
+    isSelected: false,
     },
     {
     value: "Writing",
     description: "Write down your thoughts.",
     img: 'https://images.pexels.com/photos/3059747/pexels-photo-3059747.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     id: 4,
-    selected: false,
+    isSelected: true,
     },
     {
     value: "Less Social Media",
     description: "Unplug from the Matrix.",
     img: 'https://images.pexels.com/photos/17663/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     id: 5,
-    selected: false,
+    isSelected: true,
     },
     {
     value: "Nutrition",
     description: "Increase mind and body performance.",
     img: 'https://images.pexels.com/photos/8110/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     id: 6,
-    selected: false,
+    isSelected: true,
     },
 ]
 
@@ -71,7 +71,7 @@ const Values = () => {
 
     const classes = useStyles();
 
-    const [value, setValue] = useState([]);
+    const [data, setData] = useState(initialValue);
 
     // useEffect(() => {
     //     axiosWithAuth()
@@ -79,14 +79,32 @@ const Values = () => {
     //         .then(res => setValue(res.data))
     //         .catch(err => console.log(err))
     // }, [])
+
+   const toggleItem = dataID => {
+      setData(
+        data.map(item => {
+          if (dataID === item.id) {
+            return {
+              ...item, isSelected: !item.isSelected
+            }
+          }
+          return item;
+        }
+        )
+      )
+    }
     
+    console.log("Values data", data)
     
+
     return(
         <div>
         <div style={{padding: "6%"}}>
+            <h3 style={{color: "white"}}>Your Values</h3>
             <Grid container spacing={7} display='flex' justify="space-around" align="center">
             {data.map(item => {return(
-                <Grid item xs key={data.id}>
+                (item.isSelected && 
+                <Grid item xs key={item.id}>
         <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -106,16 +124,64 @@ const Values = () => {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+          {item.isSelected ? <Button onClick={(() => toggleItem(item.id))} size="small" color="primary">
+          Remove
+        </Button> : <Button size="small" color="primary">
+          Add
+        </Button>}
+        {/* <Button size="small" color="primary">
           Add
         </Button>
         <Button size="small" color="primary">
           Remove
+        </Button> */}
+      </CardActions>
+    </Card>
+     
+    </Grid>
+            ))})}
+            </Grid>
+        </div>
+        <div style={{padding: "6%"}}>
+            <h3 style={{color: "white"}}>Add Additional Values</h3>
+            <Grid container spacing={7} display='flex' justify="space-around" align="center">
+            {data.map(item => {return(
+                (!item.isSelected && 
+                <Grid item xs key={item.id}>
+        <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={item.value}
+          height="220"
+          image={item.img}
+          title={item.value}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.value}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {item.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+          {item.isSelected ? <Button size="small" color="primary">
+          Remove
+        </Button> : <Button onClick={(() => toggleItem(item.id))} size="small" color="primary">
+          Add
+        </Button>}
+        {/* <Button size="small" color="primary">
+          Add
         </Button>
+        <Button size="small" color="primary">
+          Remove
+        </Button> */}
       </CardActions>
     </Card>
     </Grid>
-            )})}
+            ))})}
             </Grid>
         </div>
         </div>
