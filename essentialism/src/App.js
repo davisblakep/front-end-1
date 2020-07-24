9a3b45fcee765e4a97c18abe57408421def2d252
Valerie Vazquez <vnvazquez68@gmail.com>
2020-07-23 19:05:33 -0600

    goal form list

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 // import PrivateRoute from './utils/PrivateRoute';
-import HomePage from './components/pages/HomePage';
-import LoginPage from './components/pages/LoginPage';
-import Dashboard from './components/pages/Dashboard';
-import NavBar from './components/navbar/NavBar';
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import Dashboard from "./components/pages/Dashboard";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-    <div className="App">
-       <Switch>
-       <Route exact path="/">
-         <HomePage />
-       </Route>
-       <Route path="/login">
-         <LoginPage />
-       </Route>
-       {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
-       <Route path="/dashboard">
-         <Dashboard />
-       </Route>
-     </Switch> 
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
