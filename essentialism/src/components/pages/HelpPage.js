b4b917285c53fc33fb0ba1b9269021dc8f9530fb
Blake Davis <blakedavis@Blakes-MacBook-Pro.local>
2020-07-29 19:42:47 -0400

    Cleared Help Page and disabled Help button until more content is added to help section.  Not MVP so not priority

--- essentialism/src/components/pages/HelpPage.js
+++ essentialism/src/components/pages/HelpPage.js
@@ -5,16 +5,7 @@ import ReactPlayer from 'react-player'
 
 const HelpPage = () => {
       return (
-          <div style={{display: "flex", justifyItems: "center", alignItems: "center", width: "28vw", marginTop: "1%", marginLeft: "10%", marginRight: "4%"}}>
-        <div className='player-wrapper' style={{height: "66vh"}}>
-          <ReactPlayer
-            className='react-player'
-            url='https://www.youtube.com/watch?v=tsP5u6pG0RY'
-            width='375%'
-            height='123%'
-          />
-        </div>
-        </div>
+         <div></div>
       )
 }
 
